80de2cf0df74c61c7a45a019b73c6fc96f2e67cc
wangsxuan <414977601@qq.com>
2020-02-26 22:14:57 +0800

    添加了打印

--- git/src/main/java/com/baizhi/wsx/HelloWord.java
+++ git/src/main/java/com/baizhi/wsx/HelloWord.java
@@ -3,6 +3,8 @@ package com.baizhi.wsx;
 public class HelloWord {
     public static void main(String[] args) {
         System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
     }
 
 }
